b068fccda92ae0a936307a86ca8bdf70bac60200
Ziyacan Aksu <ziyacan.aksu@hotmail.com>
2024-02-22 22:53:51 +0300

    Update main.py
    connected to mongoDB localhost and inserted the parsed data

---
+++ main.py
@@ -0,0 +1,15 @@
+from Parse_Xml import parse_xml
+from db_manager import insert_product
+from pymongo import MongoClient
+
+# Parse the XML file
+mongo_uri = "mongodb://localhost:27017"
+client = MongoClient(mongo_uri)
+db = client['Lonca_DB']
+collection = db['products']
+products = parse_xml('lonca-sample.xml')
+
+# Insert products into MongoDB
+for product in products:
+    print(product)
+    insert_product(product,collection)
